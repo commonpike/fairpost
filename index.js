ac310bb827647290f9259b54ac0b8c33595aad84
pike <pike@SilverAir>
2022-12-11 12:00:50 +0100

    First tested working version

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 
 const fs = require('fs');
 require('dotenv').config();
+const fetch = require('node-fetch');
 
 const APIKEY = process.env.AYRSHARE_API_KEY;
-const defBody = "#Ayrshare feed";
 
 const appTitle = 'Ayrshare feed';
 const feedPath = './feed';
-const postFile = 'post.json';
+
+
 
 var prompt = require('prompt');
 prompt.start();
+
+// TBD move to class
+const defBody = "#Ayrshare feed";
+const postFile = 'post.json';
 
 class Post {
 
     static nextPostDate = new Date();
     static postDateInterval = 7; // days
+    static testing = false;
+    static requiresApproval = false;
     static platforms = {
-        video: ["facebook", "instagram", "youtube"],
+        video: ["facebook", "youtube","instagram"],
         image: ["twitter", "facebook", "instagram"],
         text: ["twitter", "facebook"]
     };
     id = "";
     path = "";
     body = "";
+    type= "";
     images = [];
     videos = [];
     platforms = [];
@@ -44,13 +52,20 @@ class Post {
             }
         }
         try {
-            this.body = fs.readFileSync(postDir+'/body.txt'); 
+            this.body = fs.readFileSync(this.path+'/body.txt','utf8'); 
         } catch (e) {
             this.body = defBody;
         }
         this.images = files.filter(file=>["jpg","jpeg","png"].includes(file.split('.').pop()));
         this.videos = files.filter(file=>["mp4"].includes(file.split('.').pop()));
-        
+        // TBD throw errors for mixed types
+        if (this.images.length) {
+            this.type="image";
+        } else if (this.videos.length) {
+            this.type="video";
+        } else {
+            this.type="text";
+        }
 
         this.write();
     }
@@ -61,6 +76,7 @@ class Post {
         return JSON.stringify({
             id : this.id,
             path : this.path,
+            type: this.type,
             body : this.body,
             images : this.images,
             videos : this.videos,
@@ -82,34 +98,82 @@ class Post {
 
     async  schedule() {
         this.scheduled = new Date(Post.nextPostDate);
-        if (this.images.length) {
-            await this.scheduleImagePost();
-        } else if (this.videos.length) {
-            await this.scheduleVideoPost();
+        switch (this.type) {
+            case "image":
+                await this.scheduleImagePost();
+                break;
+            case "video":
+                await this.scheduleVideoPost();
+                break;
+            default:
+                await this.scheduleTextPost();
+        }
+        if (this.result.status!=='error') {
+            Post.nextPostDate.setDate(Post.nextPostDate.getDate()+Post.postDateInterval);
         } else {
-            await this.scheduleTextPost();
+            this.scheduled=null;
         }
         this.write();
-        Post.nextPostDate.setDate(Post.nextPostDate.getDate()+Post.postDateInterval);
     }
     
     async scheduleTextPost() {
         this.platforms = Post.platforms['text'];
         this.result = await this.scheduleAyrShare([]);
+        this.id = this.result.id;
     }
     async scheduleImagePost() {
         this.platforms = Post.platforms['image'];
-        const media = this.images; // ... 
+        const media = await this.uploadMedia(this.images); 
         this.result = await this.scheduleAyrShare(media);
+        this.id = this.result.id;
     }
     async scheduleVideoPost() {
         this.platforms = Post.platforms['video'];
-        const media = this.video; // ... 
+        const media = await this.uploadMedia(this.videos); 
         this.result = await this.scheduleAyrShare(media);
+        this.id = this.result.id;
+    }
+
+    async uploadMedia(media) {
+        const urls= [];
+        for (const file of media) {
+            const buffer = fs.readFileSync(this.path+'/'+file); 
+            const ext = file.split('.').pop();
+            const res1 = await fetch("https://app.ayrshare.com/api/media/uploadUrl?fileName="+file+"&contentType="+ext, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${APIKEY}`
+                }
+            })
+            .catch(console.error);
+            const data = await res1.json();
+            console.log(data);
+            const uploadUrl = data.uploadUrl;
+            const contentType = data.contentType;
+            const accessUrl = data.accessUrl;
+            
+            const res2 = await fetch(uploadUrl, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": contentType,
+                    "Authorization": `Bearer ${APIKEY}`
+                },
+                body: buffer,
+            })
+            .catch(console.error);
+            console.log(res2);
+            urls.push(accessUrl);
+
+        }
+        return urls;
     }
 
     async scheduleAyrShare(media) {
-        return ['ok'];
+        if (Post.testing) {
+            return ['testing'];
+        }
+
+        const title = this.body.split('\n', 1)[0];
         const res = await fetch("https://app.ayrshare.com/api/post", {
           method: "POST",
           headers: {
@@ -119,12 +183,19 @@ class Post {
           body: JSON.stringify(media.length?{
             post: this.body, // required
             platforms: this.platforms, // required
-            mediaUrls: this.media, //optional
-            scheduleDate: this.scheduled
+            mediaUrls: media, 
+            scheduleDate: this.scheduled,
+            requiresApproval: Post.requiresApproval,
+            isVideo: (this.type==='video'),
+            youTubeOptions: {
+                title: title, // required max 100
+                visibility: "public" // optional def private
+            }
           }:{
             post: this.body, // required
             platforms: this.platforms, // required
-            scheduleDate: this.scheduled
+            scheduleDate: this.scheduled,
+            requiresApproval: Post.requiresApproval
           }),
         }).catch(console.error);
         return res.json();
@@ -158,7 +229,7 @@ async function main() {
     }
 
     
-    let lastPostDate = new Date('2023-01-01');
+    let lastPostDate = new Date('2022-12-04');
     const posts = [];
     getDirectories(feedPath).forEach(postDir=> {
         const post = new Post(feedPath+'/'+postDir);
