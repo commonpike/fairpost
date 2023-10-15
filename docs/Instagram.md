794dd59ea3667d012e6065299ae72ae0afda1805
pike <pike@SilverAir>
2023-10-15 11:48:01 +0200

    feat: Add instagram setup

---
+++ docs/Instagram.md
@@ -0,0 +1,111 @@
+# Platform: Instagram
+
+The `instagram` platform manage a instagram account 
+that is connected to a facebook **page* 
+using the plain facebook graph api - no extensions installed.
+
+It publishes **carousels** on that instagram account.
+
+## Setting up the Instagram platform
+
+
+### Create a new App in your facebook account
+ - create an Instagram business account
+ - connect a Facebook page to your Instagram business account
+ - go to https://developers.facebook.com/
+ - create an app that can manage pages 
+ - include the "Instagram Graph API" product as a new product 
+ - under 'settings', find your app ID 
+   - save this as `FAIRPOST_INSTAGRAM_APP_ID` in your .env
+ - under 'settings', find your app secret
+   - save this as `FAIRPOST_INSTAGRAM_APP_SECRET` in your .env
+
+
+### Find your instagram user id 
+  - go to https://www.instagram.com/web/search/topsearch/?query={username}
+  - find your fbid_v2 
+  - note the user id 
+    - save this as `FAIRPOST_INSTAGRAM_USER_ID` in your .env
+
+### Get a (short lived) Page Access Token for the page related to the instagram account you want the app to manage
+
+This is good for testing, but you'll have to refresh this token often.
+
+ - go to https://developers.facebook.com/tools/explorer/
+ - select your app 
+ - add permissions
+   - pages_manage_engagement
+   - pages_manage_posts
+   - pages_read_engagement
+   - pages_read_user_engagement
+   - publish_video
+   - business_management
+   - instagram_basic
+   - instagram_content_publish
+ - request a (short lived) page access token
+   - save this as `FAIRPOST_INSTAGRAM_PAGE_ACCESS_TOKEN` in your .env
+
+### Get a (long lived) Page Access Token for the page related to the instagram account you want the app to manage
+
+This token should last forever. It involves get a long-lived user token and then requesting the 'accounts' for your 'app scoped user id'; but this app provides a tool to help you do that: 
+
+ - go to https://developers.facebook.com/tools/explorer/
+ - select your app 
+ - add permissions
+   - pages_manage_engagement
+   - pages_manage_posts
+   - pages_read_engagement
+   - pages_read_user_engagement
+   - publish_video
+   - business_management
+   - instagram_basic
+   - instagram_content_publish
+ - request a (short lived) user access token
+ - click 'submit' to submit the default `?me` query
+   - remember the `id` in the response as your id
+ - call `./fairpost.js facebook-get-page-token
+    --app-user-id={your id} --user-token={your token}`
+   - note the token returned 
+   - save this as `FAIRPOST_INSTAGRAM_PAGE_ACCESS_TOKEN` in your .env
+
+### Enable and test the instagram platform
+ - Add 'instagram' to your `FAIRPOST_FEED_PLATFORMS` in `.env`
+ - call `./fairpost.js test-platform --platform=instagram`
+
+# Limitations 
+
+## Images 
+
+- Carousels are limited to 10 images, videos, or a mix of the two.
+- Carousel images are all cropped based on the first image in the carousel, with the default being a 1:1 aspect ratio.
+
+
+### Supported Formats
+Instagram supports the following formats:
+ - JPEG
+
+### File Size
+
+xxx
+
+# Random documentation
+
+https://developers.facebook.com/docs/instagram-api/guides/content-publishing
+
+- only jpeg
+- rate limit w endpoint
+- upload media first
+
+POST /{ig-user-id}/media — upload media and create media containers.
+POST /{ig-user-id}/media_publish — publish uploaded media using their media containers.
+GET /{ig-container-id}?fields=status_code — check media container publishing eligibility and status.
+GET /{ig-user-id}/content_publishing_limit — check app user's current publishing rate limit usage.
+
+~~~
+GET /{ig-container-id}?fields=status_code endpoint. This endpoint will return one of the following:
+
+EXPIRED — The container was not published within 24 hours and has expired.
+ERROR — The container failed to complete the publishing process.
+FINISHED — The container and its media object are ready to be published.
+IN_PROGRESS — The container is still in the publishing process.
+PUBLISHED — The container's media object has been published.
---
+++ src/platforms/Instagram.ts
@@ -0,0 +1,309 @@
+import Logger from "../Logger";
+import Platform from "../Platform";
+import { PlatformId } from ".";
+import Folder from "../Folder";
+import Post from "../Post";
+import { PostStatus } from "../Post";
+import * as fs from "fs";
+import * as path from "path";
+import * as sharp from "sharp";
+
+export default class Instagram extends Platform {
+  id: PlatformId = PlatformId.INSTAGRAM;
+  GRAPH_API_VERSION: string = "v18.0";
+
+  constructor() {
+    super();
+  }
+
+  async preparePost(folder: Folder): Promise<Post | undefined> {
+    const post = await super.preparePost(folder);
+    if (post && post.files) {
+      // instagram: 1 video for reel
+      if (post.files.video.length) {
+        Logger.trace("Removing images for instagram reel..");
+        post.files.image = [];
+        if (post.files.video.length > 1) {
+          Logger.trace("Using first video for instagram reel..");
+          post.files.video = [post.files.video[0]];
+        }
+      }
+      // instagram : scale images, jpeg only
+      for (const image of post.files.image) {
+        const metadata = await sharp(post.folder.path + "/" + image).metadata();
+        if (metadata.width > 1440) {
+          Logger.trace("Resizing " + image + " for instagram ..");
+          const extension = image.split(".")?.pop();
+          const basename = path.basename(
+            image,
+            extension ? "." + extension : "",
+          );
+          await sharp(post.folder.path + "/" + image)
+            .resize({
+              width: 1440,
+            })
+            .toFile(post.folder.path + "/_instagram-" + basename + ".JPEG");
+          post.files.image.push("_instagram-" + image);
+          post.files.image = post.files.image.filter((file) => file !== image);
+        }
+      }
+
+      // instagram: require media
+      if (post.files.image.length + post.files.video.length === 0) {
+        post.valid = false;
+      }
+      post.save();
+    }
+    return post;
+  }
+
+  
+  async publishPost(post: Post, dryrun: boolean = false): Promise<boolean> {
+    console.log("Instagram.publishPost",post,dryrun);
+    throw new Error("not implemented");
+  }
+
+  async test() {
+    return this.testUploadCarousel();
+  }
+
+  async testUploadCarousel() {
+    // upload photo to facebook
+    const photoId = (await this.uploadPhoto(
+      "/Users/pike/Desktop/test/test.jpg",
+      false,
+    ))["id"];
+    if (!photoId) return;
+
+    // get photo link
+    const photoData = (await this.get(photoId, {
+      fields: "link,images,picture",
+    })) as {
+      images: {
+        width: number;
+        height: number;
+        source: string;
+      }[];
+    };
+    if (!photoData) return;
+
+    const maxPhoto = photoData.images?.reduce(function (prev, current) {
+      return prev && prev.width > current.width ? prev : current;
+    });
+    if (!maxPhoto) return;
+
+    const photoLink = maxPhoto["source"];
+
+    // upload link to instagram
+    const uploadId = (
+      await this.postJson("%USER%/media", {
+        is_carousel_item: true,
+        image_url: photoLink,
+      })
+    )["id"];
+    if (!uploadId) return;
+
+    // create carousel
+    const carouselId = (
+      await this.postJson("%USER%/media", {
+        media_type: "CAROUSEL",
+        caption: "test",
+        children: [uploadId, uploadId].join(","),
+      })
+    )["id"];
+    if (!carouselId) return;
+
+    // publish carousel
+    const igMediaId = (
+      await this.postJson("%USER%/media_publish", {
+        creation_id: carouselId,
+      })
+    )["id"];
+    if (!igMediaId) return;
+
+    return igMediaId;
+  }
+
+  /*
+   * POST an image to the page/photos endpoint using multipart/form-data
+   *
+   * arguments:
+   * file: path to the file to post
+   *
+   * returns:
+   * id of the uploaded photo to use in post attachments
+   */
+  private async uploadPhoto(
+    file: string = "",
+    published = false,
+  ): Promise<{ id: string }> {
+    Logger.trace("Reading file", file);
+    const rawData = fs.readFileSync(file);
+    const blob = new Blob([rawData]);
+
+    const body = new FormData();
+    body.set("published", published ? "true" : "false");
+    body.set("source", blob, path.basename(file));
+
+    const result = (await this.postFormData("%PAGE%/photos", body)) as {
+      id: "string";
+    };
+
+    if (!result["id"]) {
+      throw new Error("No id returned when uploading photo");
+    }
+    return result;
+  }
+
+  // API implementation -------------------
+
+  /*
+   * Do a GET request on the graph.
+   *
+   * arguments:
+   * endpoint: the path to call
+   * query: query string as object
+   */
+
+  private async get(
+    endpoint: string = "%USER%",
+    query: { [key: string]: string } = {},
+  ): Promise<object> {
+    endpoint = endpoint.replace(
+      "%USER%",
+      process.env.FAIRPOST_INSTAGRAM_USER_ID,
+    );
+    endpoint = endpoint.replace(
+      "%PAGE%",
+      process.env.FAIRPOST_INSTAGRAM_PAGE_ID,
+    );
+
+    const url = new URL("https://graph.facebook.com");
+    url.pathname = this.GRAPH_API_VERSION + "/" + endpoint;
+    url.search = new URLSearchParams(query).toString();
+    Logger.trace("GET", url.href);
+    return await fetch(url, {
+      method: "GET",
+      headers: process.env.FAIRPOST_INSTAGRAM_PAGE_ACCESS_TOKEN
+        ? {
+            Accept: "application/json",
+            Authorization:
+              "Bearer " + process.env.FAIRPOST_INSTAGRAM_PAGE_ACCESS_TOKEN,
+          }
+        : {
+            Accept: "application/json",
+          },
+    })
+      .then((res) => this.handleApiResponse(res))
+      .catch((err) => this.handleApiError(err));
+  }
+
+  /*
+   * Do a Json POST request on the graph.
+   *
+   * arguments:
+   * endpoint: the path to call
+   * body: body as object
+   */
+
+  private async postJson(
+    endpoint: string = "%USER%",
+    body = {},
+  ): Promise<object> {
+    endpoint = endpoint.replace(
+      "%USER%",
+      process.env.FAIRPOST_INSTAGRAM_USER_ID,
+    );
+    endpoint = endpoint.replace(
+      "%PAGE%",
+      process.env.FAIRPOST_INSTAGRAM_PAGE_ID,
+    );
+
+    const url = new URL("https://graph.facebook.com");
+    url.pathname = this.GRAPH_API_VERSION + "/" + endpoint;
+    Logger.trace("POST", url.href);
+    return await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer " + process.env.FAIRPOST_INSTAGRAM_PAGE_ACCESS_TOKEN,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => this.handleApiResponse(res))
+      .catch((err) => this.handleApiError(err));
+  }
+
+  /*
+   * Do a FormData POST request on the graph.
+   *
+   * arguments:
+   * endpoint: the path to call
+   * body: body as object
+   */
+
+  private async postFormData(
+    endpoint: string,
+    body: FormData,
+  ): Promise<object> {
+    endpoint = endpoint.replace(
+      "%USER%",
+      process.env.FAIRPOST_INSTAGRAM_USER_ID,
+    );
+    endpoint = endpoint.replace(
+      "%PAGE%",
+      process.env.FAIRPOST_INSTAGRAM_PAGE_ID,
+    );
+
+    const url = new URL("https://graph.facebook.com");
+    url.pathname = this.GRAPH_API_VERSION + "/" + endpoint;
+    Logger.trace("POST", url.href);
+
+    return await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer " + process.env.FAIRPOST_INSTAGRAM_PAGE_ACCESS_TOKEN,
+      },
+      body: body,
+    })
+      .then((res) => this.handleApiResponse(res))
+      .catch((err) => this.handleApiError(err));
+  }
+
+  /*
+   * Handle api response
+   *
+   */
+  private async handleApiResponse(response: Response): Promise<object> {
+    const data = await response.json();
+    if (data.error) {
+      const error =
+        response.status +
+        ":" +
+        data.error.type +
+        "(" +
+        data.error.code +
+        "/" +
+        data.error.error_subcode +
+        ") " +
+        data.error.message;
+      Logger.error("Facebook.handleApiResponse", error);
+      throw new Error(error);
+    }
+    Logger.trace("Facebook.handleApiResponse", "success");
+    return data;
+  }
+
+  /*
+   * Handle api error
+   *
+   */
+  private handleApiError(error: Error): Promise<object> {
+    Logger.error("Facebook.handleApiError", error);
+    throw error;
+  }
+}
