11da8cb978c3f7027767565c4bb0934d903dbbc0
pike <pike@SilverAir>
2024-03-03 12:01:42 +0100

    feat: Move command to CommandHandler

--- src/cli.ts
+++ src/cli.ts
@@ -3,11 +3,7 @@
     Fairpost cli handler     
 */
 
-//import "./bootstrap-cli";
-
-import * as path from "path";
-
-import Logger from "./services/Logger";
+import CommandHandler from "./services/CommandHandler";
 import { PlatformId } from "./platforms";
 import { PostStatus } from "./models/Post";
 import User from "./models/User";
@@ -45,236 +41,17 @@ async function main() {
   let report = "";
 
   const user = new User(USER);
-  const feed = user.getFeed();
-  Logger.trace(
-    "Fairpost " + user.id + ":" + feed.id + " " + COMMAND,
-    DRY_RUN ? " dry-run" : "",
-  );
 
   try {
-    switch (COMMAND) {
-      case "get-user": {
-        result = user;
-        report = user.report();
-        break;
-      }
-      case "get-feed": {
-        result = feed;
-        report = feed.report();
-        break;
-      }
-      case "setup-platform": {
-        await feed.setupPlatform(PLATFORM);
-        result = "Success"; // or error
-        report = "Result: \n" + JSON.stringify(result, null, "\t");
-        break;
-      }
-      case "setup-platforms": {
-        await feed.setupPlatforms(PLATFORMS);
-        result = "Success"; // or error
-        report = "Result: \n" + JSON.stringify(result, null, "\t");
-        break;
-      }
-      case "get-platform": {
-        const platform = feed.getPlatform(PLATFORM);
-        report += platform.report() + "\n";
-        result = platform;
-        break;
-      }
-      case "get-platforms": {
-        const platforms = feed.getPlatforms(PLATFORMS);
-        report += platforms.length + " Platforms\n------\n";
-        platforms.forEach((platform) => {
-          report += platform.report() + "\n";
-        });
-        result = platforms;
-        break;
-      }
-      case "test-platform": {
-        result = await feed.testPlatform(PLATFORM);
-        report = "Result: \n" + JSON.stringify(result, null, "\t");
-        break;
-      }
-      case "test-platforms": {
-        result = await feed.testPlatforms(PLATFORMS);
-        report = "Result: \n" + JSON.stringify(result, null, "\t");
-        break;
-      }
-      case "refresh-platform": {
-        result = await feed.refreshPlatform(PLATFORM);
-        report = "Result: \n" + JSON.stringify(result, null, "\t");
-        break;
-      }
-      case "refresh-platforms": {
-        result = await feed.refreshPlatforms(PLATFORMS);
-        report = "Result: \n" + JSON.stringify(result, null, "\t");
-        break;
-      }
-      case "get-folder": {
-        const folder = feed.getFolder(FOLDER);
-        if (folder) {
-          report += folder.report() + "\n";
-          result = folder;
-        } else {
-          report += "not found:" + FOLDER + "\n";
-        }
-        break;
-      }
-      case "get-folders": {
-        const folders = feed.getFolders(FOLDERS);
-        report += folders.length + " Folders\n------\n";
-        folders.forEach((folder) => {
-          report += folder.report() + "\n";
-        });
-        result = folders;
-        break;
-      }
-      case "get-post": {
-        const post = feed.getPost(FOLDER, PLATFORM);
-        if (post) {
-          report += post.report();
-          result = post;
-        } else {
-          report += "Not found:" + FOLDER + ":" + PLATFORM;
-        }
-        break;
-      }
-      case "get-posts": {
-        const allposts = feed.getPosts({
-          folders: FOLDERS,
-          platforms: PLATFORMS,
-          status: STATUS,
-        });
-        report += allposts.length + " Posts\n------\n";
-        allposts.forEach((post) => {
-          report += post.report();
-        });
-        result = allposts;
-        break;
-      }
-      case "prepare-post": {
-        const preppost = await feed.preparePost(FOLDER, PLATFORM);
-        if (preppost) {
-          report += preppost.report();
-          result = preppost;
-        } else {
-          report += "Failed: " + FOLDER + ":" + PLATFORM;
-        }
-        break;
-      }
-      case "prepare-posts": {
-        const prepposts = await feed.preparePosts({
-          folders: FOLDERS,
-          platforms: PLATFORMS,
-        });
-        prepposts.forEach((post) => {
-          report += post.report();
-        });
-        result = prepposts;
-        break;
-      }
-      case "schedule-post": {
-        const schedpost = feed.schedulePost(FOLDER, PLATFORM, new Date(DATE));
-        report += schedpost.report();
-        result = schedpost;
-        break;
-      }
-      case "schedule-posts": {
-        const schedposts = feed.schedulePosts(
-          {
-            folders: FOLDERS,
-            platforms: PLATFORMS,
-          },
-          new Date(DATE),
-        );
-        schedposts.forEach((post) => {
-          report += post.report();
-        });
-        result = schedposts;
-        break;
-      }
-      case "publish-post": {
-        const pubpost = await feed.publishPost(FOLDER, PLATFORM, DRY_RUN);
-        report += pubpost.report();
-        result = pubpost;
-        break;
-      }
-      case "publish-posts": {
-        const pubposts = await feed.publishPosts(
-          {
-            folders: FOLDERS,
-            platforms: PLATFORMS,
-          },
-          DRY_RUN,
-        );
-        pubposts.forEach((post) => {
-          report += post.report();
-        });
-        result = pubposts;
-        break;
-      }
-
-      /* feed planning */
-      case "schedule-next-posts": {
-        const nextposts = feed.scheduleNextPosts(
-          DATE ? new Date(DATE) : undefined,
-          {
-            folders: FOLDERS,
-            platforms: PLATFORMS,
-          },
-        );
-        nextposts.forEach((post) => {
-          report += post.report();
-        });
-        result = nextposts;
-        break;
-      }
-      case "publish-due-posts": {
-        const dueposts = await feed.publishDuePosts(
-          {
-            folders: FOLDERS,
-            platforms: PLATFORMS,
-          },
-          DRY_RUN,
-        );
-        dueposts.forEach((post) => {
-          report += post.report();
-        });
-        result = dueposts;
-        break;
-      }
-
-      default: {
-        const cmd = path.basename(process.argv[1]);
-        result = [
-          "# basic commands:",
-          `${cmd} help`,
-          `${cmd} get-feed [--config=xxx]`,
-          `${cmd} setup-platform --platform=xxx`,
-          `${cmd} setup-platforms [--platforms=xxx,xxx]`,
-          `${cmd} test-platform --platform=xxx`,
-          `${cmd} test-platforms [--platforms=xxx,xxx]`,
-          `${cmd} refresh-platform --platform=xxx`,
-          `${cmd} refresh-platforms [--platforms=xxx,xxx]`,
-          `${cmd} get-platform --platform=xxx`,
-          `${cmd} get-platforms [--platforms=xxx,xxx]`,
-          `${cmd} get-folder --folder=xxx`,
-          `${cmd} get-folders [--folders=xxx,xxx]`,
-          `${cmd} get-post --post=xxx:xxx`,
-          `${cmd} get-posts [--status=xxx] [--folders=xxx,xxx] [--platforms=xxx,xxx] `,
-          `${cmd} prepare-post --post=xxx:xxx`,
-          `${cmd} schedule-post --post=xxx:xxx --date=xxxx-xx-xx `,
-          `${cmd} schedule-posts [--folders=xxx,xxx] [--platforms=xxx,xxx] --date=xxxx-xx-xx`,
-          `${cmd} publish-post --post=xxx:xxx [--dry-run]`,
-          `${cmd} publish-posts [--folders=xxx,xxx] [--platforms=xxx,xxx]`,
-          "\n# feed planning:",
-          `${cmd} prepare-posts [--folders=xxx,xxx] [--platforms=xxx,xxx]`,
-          `${cmd} schedule-next-post [--date=xxxx-xx-xx] [--folders=xxx,xxx] [--platforms=xxx,xxx] `,
-          `${cmd} publish-due-posts [--folders=xxx,xxx] [--platforms=xxx,xxx] [--dry-run]`,
-        ];
-        (result as string[]).forEach((line) => (report += "\n" + line));
-      }
-    }
+    ({result,report} = await CommandHandler.execute(user,COMMAND,{
+      dryrun: DRY_RUN,
+      platforms: PLATFORMS,
+      platform: PLATFORM,
+      folders: FOLDERS,
+      folder: FOLDER,
+      date: DATE ? new Date(DATE) : undefined,
+      status: STATUS
+    }));
   } catch (e) {
     console.error((e as Error).message ?? e);
   }
---
+++ src/services/CommandHandler.ts
@@ -0,0 +1,330 @@
+/**
+ * CommandHandler - minimalist singleton service
+ *
+ * Executes commands on behalf of a user
+ *
+ */
+
+import Logger from "../services/Logger";
+import { PlatformId } from "../platforms";
+import { PostStatus } from "../models/Post";
+import User from "../models/User";
+
+class CommandHandler {
+  static instance: CommandHandler;
+  constructor() {
+    if (CommandHandler.instance) {
+      throw new Error("CommandHandler: call getInstance() instead");
+    }
+  }
+  static getInstance(): CommandHandler {
+    if (!CommandHandler.instance) {
+      CommandHandler.instance = new CommandHandler();
+    }
+    return CommandHandler.instance;
+  }
+  async execute(user: User, command: string = "help", args: CommandArguments = {}): Promise<{
+    result: any,
+    report: string
+  }> {
+
+    let result: unknown;
+    let report = "";
+
+    const feed = user.getFeed();
+    Logger.trace(
+      "Fairpost " + user.id + ":" + feed.id + " " + command,
+      args.dryrun ? " dry-run" : "",
+    );
+
+    switch (command) {
+      case "get-user": {
+        result = user;
+        report = user.report();
+        break;
+      }
+      case "get-feed": {
+        result = feed;
+        report = feed.report();
+        break;
+      }
+      case "setup-platform": {
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        await feed.setupPlatform(args.platform);
+        result = "Success"; // or error
+        report = "Result: \n" + JSON.stringify(result, null, "\t");
+        
+        break;
+      }
+      case "setup-platforms": {
+        await feed.setupPlatforms(args.platforms);
+        result = "Success"; // or error
+        report = "Result: \n" + JSON.stringify(result, null, "\t");
+        break;
+      }
+      case "get-platform": {
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        const platform = feed.getPlatform(args.platform);
+        report += platform.report() + "\n";
+        result = platform;
+        break;
+      }
+      case "get-platforms": {
+        const platforms = feed.getPlatforms(args.platforms);
+        report += platforms.length + " Platforms\n------\n";
+        platforms.forEach((platform) => {
+          report += platform.report() + "\n";
+        });
+        result = platforms;
+        break;
+      }
+      case "test-platform": {
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        result = await feed.testPlatform(args.platform);
+        report = "Result: \n" + JSON.stringify(result, null, "\t");
+        break;
+      }
+      case "test-platforms": {
+        result = await feed.testPlatforms(args.platforms);
+        report = "Result: \n" + JSON.stringify(result, null, "\t");
+        break;
+      }
+      case "refresh-platform": {
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        result = await feed.refreshPlatform(args.platform);
+        report = "Result: \n" + JSON.stringify(result, null, "\t");
+        break;
+      }
+      case "refresh-platforms": {
+        result = await feed.refreshPlatforms(args.platforms);
+        report = "Result: \n" + JSON.stringify(result, null, "\t");
+        break;
+      }
+      case "get-folder": {
+        if (!args.folder) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folder");
+        }
+        const folder = feed.getFolder(args.folder);
+        if (folder) {
+          report += folder.report() + "\n";
+          result = folder;
+        } else {
+          report += "not found:" + args.folder + "\n";
+        }
+        break;
+      }
+      case "get-folders": {
+        const folders = feed.getFolders(args.folders);
+        report += folders.length + " Folders\n------\n";
+        folders.forEach((folder) => {
+          report += folder.report() + "\n";
+        });
+        result = folders;
+        break;
+      }
+      case "get-post": {
+        if (!args.folder) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folder");
+        }
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        const post = feed.getPost(args.folder, args.platform);
+        if (post) {
+          report += post.report();
+          result = post;
+        } else {
+          report += "Not found:" + args.folder + ":" + args.platform;
+        }
+        break;
+      }
+      case "get-posts": {
+        const allposts = feed.getPosts({
+          folders: args.folders,
+          platforms: args.platforms,
+          status: args.status,
+        });
+        report += allposts.length + " Posts\n------\n";
+        allposts.forEach((post) => {
+          report += post.report();
+        });
+        result = allposts;
+        break;
+      }
+      case "prepare-post": {
+        if (!args.folder) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folder");
+        }
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        const preppost = await feed.preparePost(args.folder, args.platform);
+        if (preppost) {
+          report += preppost.report();
+          result = preppost;
+        } else {
+          report += "Failed: " + args.folder + ":" + args.platform;
+        }
+        break;
+      }
+      case "prepare-posts": {
+        const prepposts = await feed.preparePosts({
+          folders: args.folders,
+          platforms: args.platforms,
+        });
+        prepposts.forEach((post) => {
+          report += post.report();
+        });
+        result = prepposts;
+        break;
+      }
+      case "schedule-post": {
+        if (!args.folder) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folder");
+        }
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        if (!args.date) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: date");
+        }
+        const schedpost = feed.schedulePost(args.folder, args.platform, args.date);
+        report += schedpost.report();
+        result = schedpost;
+        break;
+      }
+      case "schedule-posts": {
+        if (!args.folders) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folders");
+        }
+        if (!args.date) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: date");
+        }
+        const schedposts = feed.schedulePosts(
+          {
+            folders: args.folders,
+            platforms: args.platforms,
+          },
+          new Date(args.date),
+        );
+        schedposts.forEach((post) => {
+          report += post.report();
+        });
+        result = schedposts;
+        break;
+      }
+      case "publish-post": {
+        if (!args.folder) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folder");
+        }
+        if (!args.platform) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: platform");
+        }
+        const pubpost = await feed.publishPost(args.folder, args.platform, args.dryrun);
+        report += pubpost.report();
+        result = pubpost;
+        break;
+      }
+      case "publish-posts": {
+        if (!args.folders) {
+          throw Logger.error("CommandHandler "+command,"Missing argument: folders");
+        }
+        const pubposts = await feed.publishPosts(
+          {
+            folders: args.folders,
+            platforms: args.platforms,
+          },
+          args.dryrun,
+        );
+        pubposts.forEach((post) => {
+          report += post.report();
+        });
+        result = pubposts;
+        break;
+      }
+
+      /* feed planning */
+      case "schedule-next-posts": {
+        const nextposts = feed.scheduleNextPosts(
+          args.date ? new Date(args.date) : undefined,
+          {
+            folders: args.folders,
+            platforms: args.platforms,
+          },
+        );
+        nextposts.forEach((post) => {
+          report += post.report();
+        });
+        result = nextposts;
+        break;
+      }
+      case "publish-due-posts": {
+        const dueposts = await feed.publishDuePosts(
+          {
+            folders: args.folders,
+            platforms: args.platforms,
+          },
+          args.dryrun,
+        );
+        dueposts.forEach((post) => {
+          report += post.report();
+        });
+        result = dueposts;
+        break;
+      }
+
+      default: {
+        const cmd = 'fairpost:';
+        result = [
+          "# basic commands:",
+          `${cmd} help`,
+          `${cmd} get-feed [--config=xxx]`,
+          `${cmd} setup-platform --platform=xxx`,
+          `${cmd} setup-platforms [--platforms=xxx,xxx]`,
+          `${cmd} test-platform --platform=xxx`,
+          `${cmd} test-platforms [--platforms=xxx,xxx]`,
+          `${cmd} refresh-platform --platform=xxx`,
+          `${cmd} refresh-platforms [--platforms=xxx,xxx]`,
+          `${cmd} get-platform --platform=xxx`,
+          `${cmd} get-platforms [--platforms=xxx,xxx]`,
+          `${cmd} get-folder --folder=xxx`,
+          `${cmd} get-folders [--folders=xxx,xxx]`,
+          `${cmd} get-post --post=xxx:xxx`,
+          `${cmd} get-posts [--status=xxx] [--folders=xxx,xxx] [--platforms=xxx,xxx] `,
+          `${cmd} prepare-post --post=xxx:xxx`,
+          `${cmd} schedule-post --post=xxx:xxx --date=xxxx-xx-xx `,
+          `${cmd} schedule-posts [--folders=xxx,xxx] [--platforms=xxx,xxx] --date=xxxx-xx-xx`,
+          `${cmd} publish-post --post=xxx:xxx [--dry-run]`,
+          `${cmd} publish-posts [--folders=xxx,xxx] [--platforms=xxx,xxx]`,
+          "\n# feed planning:",
+          `${cmd} prepare-posts [--folders=xxx,xxx] [--platforms=xxx,xxx]`,
+          `${cmd} schedule-next-post [--date=xxxx-xx-xx] [--folders=xxx,xxx] [--platforms=xxx,xxx] `,
+          `${cmd} publish-due-posts [--folders=xxx,xxx] [--platforms=xxx,xxx] [--dry-run]`,
+        ];
+        (result as string[]).forEach((line) => (report += "\n" + line));
+      }
+    }
+    return {
+      result: result,
+      report: report
+    }
+  }
+}
+interface CommandArguments {
+  dryrun?: boolean,
+  platforms?: PlatformId[],
+  platform?: PlatformId,
+  folders?: string[],
+  folder?: string,
+  date?: Date,
+  status?: PostStatus
+}
+export default CommandHandler.getInstance();
