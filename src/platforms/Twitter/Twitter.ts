08bed820e99b953b0dcef46f591a96c9b9fa2b70
pike <pike@GreyMono.local>
2025-02-16 11:04:12 +0100

    feat: Minor updates

--- src/platforms/Twitter/Twitter.ts
+++ src/platforms/Twitter/Twitter.ts
@@ -207,14 +207,14 @@ export default class Twitter extends Platform {
       accessToken: this.user.get("app", "TWITTER_OA1_ACCESS_TOKEN"),
       accessSecret: this.user.get("app", "TWITTER_OA1_ACCESS_SECRET"),
     });
-    const mediaIds = [];
+    const mediaIds = new Array() as [string] | [string,string] | [string,string,string]| [string, string, string, string];
 
     const additionalOwner = this.user.get(
       "settings",
       "TWITTER_OA1_ADDITIONAL_OWNER",
       "",
     );
-    for (const image of post.getFiles(FileGroup.IMAGE)) {
+    for (const image of post.getFiles(FileGroup.IMAGE).splice(0,4)) {
       const path = post.getFilePath(image.name);
       this.user.trace("Uploading " + path + "...");
       try {
@@ -241,7 +241,9 @@ export default class Twitter extends Platform {
       this.user.trace("Tweeting " + post.id + "...");
       const result = await client2.v2.tweet({
         text: post.getCompiledBody(),
-        media: { media_ids: mediaIds },
+        media: { 
+          media_ids: mediaIds
+        },
       });
       if (result.errors) {
         throw this.user.error(result.errors.join());
